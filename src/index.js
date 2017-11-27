261977bf3a3751806595f3330c363f9bc28f3eb5
Valery Kobzar <valery.kobzar@rcntec.com>
2017-11-27 23:23:34 +0300

    added eslint, travis config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import postcss from 'postcss'
 import multimatch from 'multimatch'
 
 
+const error = () => {
+  throw new TypeError('The plugins parameter must be function or object or Array')
+}
+
 export default postcss.plugin('postcss-exclude-files', opts => {
   const { filter, plugins } = opts || {}
 
@@ -12,34 +16,16 @@ export default postcss.plugin('postcss-exclude-files', opts => {
   const typeOfPlugin = typeof plugins
 
   if (!plugins || typeOfPlugin !== 'function' && typeOfPlugin !== 'object' && !Array.isArray(plugins)) {
-    throw new TypeError('The plugins parameter must be function or object or Array')
+    error()
   }
 
   return (root, result) => {
     if (multimatch(root.source.input.file, filter).length === 0) {
       if (typeof plugins === 'function') {
-        //return plugins().process(root).then(response =>
-        //  response.messages.forEach(msg => result.messages.push(msg))
-        //)
-        //if (typeof plugins.processs === 'function') {
-        //  console.log('11111')
           return postcss([plugins]).process(root).then(response =>
             response.messages.forEach(msg => result.messages.push(msg))
           )
-        //} else {
-        //  console.log('22222', plugins, typeof plugins)
-        //  return postcss([plugins()]).process(root).then(response =>
-        //    response.messages.forEach(msg => result.messages.push(msg))
-        //  )
-        //}
       } else if (Array.isArray(plugins) || typeof plugins === 'object') {
-        //const pluginList = plugins.map(plugin => {
-        //  if (plugins.hasOwnProperty('process') && typeof plugins.processs === 'function') {
-        //    return plugin()
-        //  } else {
-        //    return plugin
-        //  }
-        //})
         return postcss(plugins).process(root).then(response =>
           response.messages.forEach(msg => result.messages.push(msg))
         )
@@ -55,11 +41,3 @@ export default postcss.plugin('postcss-exclude-files', opts => {
     })
   }
 })
-
-//postcssExcludeFiles.process = (css, opts) => {
-//  opts = opts || { filter: '**/node_modules/**', plugins: [] }
-//  const processor = postcss([postcssExcludeFiles(opts)])
-//  return processor.process(css, opts)
-//}
-
-//module.exports = postcssExcludeFiles
